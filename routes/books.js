2dd1400ea2eedf76353dbb87fca4d4049dfd30ad
Mateusz Buczek <mateusz.buczek@schibsted.pl>
2016-01-26 15:20:40 +0100

    Using request with promises

--- routes/books.js
+++ routes/books.js
@@ -1,20 +1,21 @@
 var express = require('express');
-var request = require('request');
+var request = require('request-promise');
 
 var router = express.Router();
 var apiUrl = 'https://www.googleapis.com/books/v1/volumes';
 
 router.get('/:isbn', function(req, res, next) {
-	request(apiUrl + '?q=isbn:' + req.params.isbn, function(error, apiResp, body) {
-		if(!error && apiResp.statusCode == 200) {
-			var json = JSON.parse(body);
-			var title = json.items[0].volumeInfo.title;
-			var cover = json.items[0].volumeInfo.imageLinks.thumbnail;
-			res.render('books', {cover: cover, title: title});
-		} else {
-			next(error);
-		}
-	});
+	request({url: apiUrl, qs: {'q': 'isbn:' + req.params.isbn}, json: true})
+		.then(function(reply) {
+			if(reply.totalItems > 0) {
+				var title = reply.items[0].volumeInfo.title;
+				var cover = reply.items[0].volumeInfo.imageLinks.thumbnail;
+				res.render('books', {cover: cover, title: title});
+			} else {
+				res.status(404).send('Book not found');
+			}
+		})
+		.catch(next);
 });
 
 module.exports = router;
